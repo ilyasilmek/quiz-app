e283bcb003b11c4f546da958dd99efa91b3fa4ad
ilyasilmek <ilyas-ilmek@hotmail.com>
2026-08-30 13:02:42 +0300

    build: align admin app with Kotlin Compose compiler plugin

--- admin-app/app/build.gradle.kts
+++ admin-app/app/build.gradle.kts
@@ -1,11 +1,13 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
     namespace = "com.bilgiarenasi.admin"
     compileSdk = 36
+
     defaultConfig {
         applicationId = "com.bilgiarenasi.admin"
         minSdk = 24
@@ -13,8 +15,13 @@ android {
         versionCode = 1
         versionName = "0.1.0"
     }
+
     buildFeatures { compose = true }
-    composeOptions { kotlinCompilerExtensionVersion = "2.2.10" }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions { jvmTarget = "17" }
 }
 
 dependencies {
@@ -24,4 +31,5 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
+    debugImplementation("androidx.compose.ui:ui-tooling")
 }
