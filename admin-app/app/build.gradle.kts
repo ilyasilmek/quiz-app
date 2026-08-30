796b509fa3d597b866351438a28051ae3e7ddad9
ilyasilmek <ilyas-ilmek@hotmail.com>
2026-08-30 14:18:14 +0300

    build: add admin API dependencies

--- admin-app/app/build.gradle.kts
+++ admin-app/app/build.gradle.kts
@@ -29,5 +29,9 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
+    implementation("com.squareup.retrofit2:retrofit:2.12.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.12.0")
+    implementation("com.squareup.moshi:moshi:1.15.2")
+    implementation("com.squareup.moshi:moshi-kotlin:1.15.2")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
